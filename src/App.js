6492c438d8450444c2ffce84ed0e3e26f78fda02
M. M. Bapin <m.bapin@konasl.com>
2022-09-05 18:16:24 +0600

    banner added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 import './static/styles/App.scss'
 import Header from './components/navigation/Header';
+import HomeSlider from './components/home-slider/HomeSlider';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <HomeSlider />
     </div>
   );
 }
---
+++ src/components/home-slider/HomeSlider.js
@@ -0,0 +1,83 @@
+import React from 'react'
+import Slider from "react-slick";
+import { Typography } from '@mui/material';
+
+const HomeSlider = () => {
+  var settings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 3000,
+    autoplaySpeed: 3000,
+    // cssEase: "linear",
+    initialSlide: 0,
+    className: "center sliderwrap",
+    arrows: false,
+    // swipeToSlide: true,
+    // centerMode: true,
+    // centerPadding: "60px",
+
+    // responsive: [
+    //   {
+    //     breakpoint: 1024,
+    //     settings: {
+    //       slidesToShow: 3,
+    //       slidesToScroll: 3,
+    //       infinite: true,
+    //       dots: true
+    //     }
+    //   },
+    //   {
+    //     breakpoint: 600,
+    //     settings: {
+    //       slidesToShow: 2,
+    //       slidesToScroll: 2,
+    //       initialSlide: 2
+    //     }
+    //   },
+    //   {
+    //     breakpoint: 480,
+    //     settings: {
+    //       slidesToShow: 1,
+    //       slidesToScroll: 1
+    //     }
+    //   }
+    // ]
+  };
+
+  const sliderData = [
+    {
+      title: 'Build Payment Ecosystem with KONA PLATE',
+      desc: 'Instantly build your cards & payment platform you need with KONA open API platform'
+    },
+    {
+      title: 'Build Payment Ecosystem with KONA PLATE',
+      desc: 'Instantly build your cards & payment platform you need with KONA open API platform'
+    },
+    {
+      title: 'Build Payment Ecosystem with KONA PLATE',
+      desc: 'Instantly build your cards & payment platform you need with KONA open API platform'
+    }
+  ]
+  return (
+      <div className='mainslider'>
+        {/* <h2> Responsive </h2> */}
+        <Slider {...settings}>
+          {sliderData?.map((item, index) => (
+            <div className='sliderwrap__listitem'>
+              <Typography component='h1' className='sliderwrap__title'>
+                {item.title}
+              </Typography>
+              <Typography component='p' className='sliderwrap__desc'>
+                {item.desc}
+              </Typography>
+            </div>
+          ))}
+        </Slider>
+      </div>
+  )
+}
+
+export default HomeSlider
